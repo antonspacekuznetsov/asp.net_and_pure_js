04f774c164d9184db6cefd93519d2a46985d7cf2
Admin <Admin>
2017-03-22 18:43:37 +0300

    first commit

---
+++ GetJsonFromAspNet/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GetJsonFromAspNet.Repositories;
+using Newtonsoft.Json;
+using GetJsonFromAspNet.ActionResults;
+
+namespace GetJsonFromAspNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetPeopleViaJson()
+        {
+            var peopleRepository = new PeopleRepository();
+            var people = peopleRepository.GetPeople();
+
+            return Json(people);
+        }
+
+
+        public ActionResult GetTime()
+        {
+
+            var time = new { Title = "Время", Time = DateTime.Now.ToString() };
+            return Json(time);
+        }
+    }
+}
